8b595eb20b9bf1715f5b6a8922db572abfac86da
Anjan Naskar <36865487+anjannaskar93@users.noreply.github.com>
2021-08-21 21:18:10 +0530

    git commit

---
+++ 2/Metadata.sh
@@ -0,0 +1,2 @@
+TOKEN=`curl --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 21600"
+curl --write-out "\n" --request GET "http://169.254.169.254/latest/dynamic/instance-identity/document" --header "X-aws-ec2-metadata-token: $TOKEN" --output JSON
